e930a6bffa8025c8d61716e7d44919ee337d638c
SomkamolV <somkamolv@hotmail.com>
2026-03-01 16:34:52 +0700

    create InitTable and another migration file that SET search_path TO public to $user. So you dont have to type 'public.' prefix.

---
+++ Migrations/20260301071223_initTables.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace evacPlanMoni.Migrations
+{
+    /// <inheritdoc />
+    public partial class initTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EvacuationPlans",
+                columns: table => new
+                {
+                    ZoneId = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    VehicleId = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    ETAHours = table.Column<double>(type: "double precision", nullable: false),
+                    PeopleToEvacuate = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EvacuationPlans", x => new { x.ZoneId, x.VehicleId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EvacuationStatuses",
+                columns: table => new
+                {
+                    ZoneId = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    TotalEvacuated = table.Column<int>(type: "integer", nullable: false),
+                    RemainingPeople = table.Column<int>(type: "integer", nullable: false),
+                    LastVehicleUsed = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EvacuationStatuses", x => x.ZoneId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EvacuationZones",
+                columns: table => new
+                {
+                    ZoneId = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    Longitude = table.Column<double>(type: "double precision", nullable: false),
+                    TotalPeople = table.Column<int>(type: "integer", nullable: false),
+                    UrgencyLevel = table.Column<int>(type: "integer", nullable: false, defaultValue: 1)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EvacuationZones", x => x.ZoneId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Vehicles",
+                columns: table => new
+                {
+                    VehicleId = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Capacity = table.Column<int>(type: "integer", nullable: false),
+                    Type = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Latitude = table.Column<double>(type: "double precision", nullable: false),
+                    Longitude = table.Column<double>(type: "double precision", nullable: false),
+                    Speed = table.Column<double>(type: "double precision", nullable: false),
+                    IsAvailable = table.Column<bool>(type: "boolean", nullable: false, defaultValue: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Vehicles", x => x.VehicleId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EvacuationZones_UrgencyLevel",
+                table: "EvacuationZones",
+                column: "UrgencyLevel");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EvacuationPlans");
+
+            migrationBuilder.DropTable(
+                name: "EvacuationStatuses");
+
+            migrationBuilder.DropTable(
+                name: "EvacuationZones");
+
+            migrationBuilder.DropTable(
+                name: "Vehicles");
+        }
+    }
+}
---
+++ Migrations/20260301082240_setSearchPath.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace evacPlanMoni.Migrations
+{
+    /// <inheritdoc />
+    public partial class setSearchPath : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Sets the default schema for all entities in the model
+            // This is only for postgres. In appsettings connection string also need `SearchPath=public;`.
+            migrationBuilder.Sql("ALTER DATABASE \"EvacuationDb\" SET search_path TO public, \"$user\";");
+            // apply to the specific user
+            migrationBuilder.Sql("ALTER ROLE \"postgres\" SET search_path TO public, \"$user\";");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            //Revert
+            migrationBuilder.Sql("ALTER DATABASE \"EvacuationDb\" RESET search_path;");
+            migrationBuilder.Sql("ALTER ROLE \"postgres\" RESET search_path;");
+        }
+    }
+}
