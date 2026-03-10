2c0d2b3c233273e2a0e13c446fd78c09bd6d8924
SomkamolV <somkamolv@hotmail.com>
2026-03-10 22:11:42 +0700

    new migration => new column name

---
+++ Migrations/20260310150935_SyncLatestModelChanges.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace evacPlanMoni.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncLatestModelChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ETAHours",
+                table: "EvacuationPlans");
+
+            migrationBuilder.RenameColumn(
+                name: "PeopleToEvacuate",
+                table: "EvacuationPlans",
+                newName: "NumberOfPeople");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ETA",
+                table: "EvacuationPlans",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ETA",
+                table: "EvacuationPlans");
+
+            migrationBuilder.RenameColumn(
+                name: "NumberOfPeople",
+                table: "EvacuationPlans",
+                newName: "PeopleToEvacuate");
+
+            migrationBuilder.AddColumn<double>(
+                name: "ETAHours",
+                table: "EvacuationPlans",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
